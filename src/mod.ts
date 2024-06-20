d10aff9d0ba5eb5ab8b87a886c6d69606d6a5eaa
phothinmg <phothinmg@gmail.com>
2024-06-21 00:10:44 +0630

    new file:   LICENSE

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,15 @@
+/**
+ * # Hono Image
+
+    ## About
+
+    Related components , functions and middlewares of image for [HONO](https://hono.dev/) , that supported
+    any JavaScript runtime but this package only focus on [DENO](https://deno.com/).
+
+    The package is under developing state now.
+    
+ */
+
 import imageToUri from "./image-to-uri/mod.ts";
 import ImageToUri from "./image-to-uri/Index.tsx";
 import {SocialIcon} from "./social-icon/Index.tsx";
